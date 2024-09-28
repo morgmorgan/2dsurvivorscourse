ffd23d214f7959956a3869b66874df66cb5f3d06
Morgan Nemeth <morgan.nemeth210@gmail.com>
2024-09-28 00:26:54 -0700

    Lesson 38

---
+++ scenes/component/death_component.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@export var health_component : HealthComponent
+@export var sprite : Sprite2D
+
+func _ready():
+	$ParticlesCPU.texture = sprite.texture
+	health_component.died.connect(on_died)
+	
+func on_died():
+	if owner == null || not owner is Node2D:
+		return
+	
+	var spawn_pos = owner.global_position
+	
+	var entities = get_tree().get_first_node_in_group("entities_layer")
+	get_parent().remove_child(self)
+	entities.add_child(self)
+	
+	global_position = spawn_pos
+	
+	$AnimationPlayer.play("default")
+	
--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -3,12 +3,18 @@ extends CharacterBody2D
 const MAX_SPEED = 40
 
 @onready var health_component : HealthComponent = $HealthComponent
+@onready var visuals = $Visuals
 
 
 func _process(_delta):
 	var direction = get_direction_to_player()
 	velocity = direction * MAX_SPEED
 	move_and_slide()
+	
+	# Face Movement Direction
+	var move_sign = sign(velocity.x)
+	if move_sign != 0:
+		visuals.scale.x = move_sign
 
 
 func get_direction_to_player():
