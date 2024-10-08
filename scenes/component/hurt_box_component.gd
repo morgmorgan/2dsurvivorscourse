91e0af2118109765e8e1483d322b27dc1f6657bb
Morgan Nemeth <morgan.nemeth210@gmail.com>
2024-10-07 22:28:47 -0700

    Lesson 44

--- scenes/component/hurt_box_component.gd
+++ scenes/component/hurt_box_component.gd
@@ -2,6 +2,7 @@ extends Area2D
 class_name HurtboxComponent
 
 @export var health_component : HealthComponent
+@export var floating_text_scene : PackedScene
 
 func _ready():
 	area_entered.connect(on_area_entered)
@@ -15,3 +16,9 @@ func on_area_entered(other : Area2D):
 		
 	var hitbox_component : HitboxComponent = other
 	health_component.damage(hitbox_component.damage)
+	
+	var floating_text : FloatingText = floating_text_scene.instantiate()
+	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
+	
+	floating_text.global_position = global_position + (Vector2.UP * 16)
+	floating_text.start(str(hitbox_component.damage))
--- scenes/game_object/experience_vial/experience_vial.gd
+++ scenes/game_object/experience_vial/experience_vial.gd
@@ -1,8 +1,43 @@
 extends Node2D
 
+@onready var collision_shape_2d = $Area2D/CollisionShape2D
+@onready var sprite_2d = $Sprite2D
+
 func _ready():
 	$Area2D.area_entered.connect(on_area_entered)
 
 func on_area_entered(_other : Area2D):
+	# Disable collisions
+	Callable(disable_collision).call_deferred()
+	
+	# Disable floating animation
+	$AnimationPlayer.current_animation = "RESET"
+	
+	var tween = create_tween()
+	tween.set_parallel()
+	tween.tween_method(tween_collect.bind(global_position), 0.0, 1.0, 0.5)\
+	.set_ease(Tween.EASE_IN)\
+	.set_trans(Tween.TRANS_BACK)
+	tween.tween_property(sprite_2d, "scale", Vector2.ZERO, .15).set_delay(.35)
+	tween.chain()
+	tween.tween_callback(collect)
+
+func collect():
 	GameEvents.emit_experience_vial_collected(1)
 	queue_free()
+
+func tween_collect(percent : float, start_pos : Vector2):
+	var player : Node2D = get_tree().get_first_node_in_group("player")
+	if player == null:
+		return
+	
+	# Set position
+	global_position = start_pos.lerp(player.global_position, percent)
+	
+	# Set rotation
+	var direction_from_start = player.global_position - start_pos
+	var target_rotation = direction_from_start.angle() - deg_to_rad(90)
+	rotation = lerp_angle(rotation, target_rotation, 1-exp(-2 * get_process_delta_time()))
+
+func disable_collision():
+	collision_shape_2d.disabled = true
