e064eeacae53a33a0dd7d1c2d9fe008f5cab8054
Morgan Nemeth <morgan.nemeth210@gmail.com>
2024-10-14 01:36:52 -0700

    Lesson 48

--- scenes/UI/ability_upgrade_card.gd
+++ scenes/UI/ability_upgrade_card.gd
@@ -3,8 +3,8 @@ class_name AbilityUpgradeCard
 
 signal selected
 
-@onready var name_label : Label = $VBoxContainer/NameLabel
-@onready var description_label : Label = $VBoxContainer/DescriptionLabel
+@onready var name_label : Label = %NameLabel
+@onready var description_label : Label = %DescriptionLabel
 
 func _ready():
 	gui_input.connect(on_gui_input)
