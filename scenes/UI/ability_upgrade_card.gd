d8c116dc74f038a8fe8a3483622a164d0f497f56
Morgan Nemeth <morgan.nemeth210@gmail.com>
2024-10-18 00:17:47 -0700

    lesson 49

--- scenes/UI/ability_upgrade_card.gd
+++ scenes/UI/ability_upgrade_card.gd
@@ -6,13 +6,44 @@ signal selected
 @onready var name_label : Label = %NameLabel
 @onready var description_label : Label = %DescriptionLabel
 
+var ignore_input : bool = false
+
 func _ready():
 	gui_input.connect(on_gui_input)
+	mouse_entered.connect(on_mouse_entered)
+	
+func play_in(delay : float = 0.0):
+	modulate = Color.TRANSPARENT
+	await get_tree().create_timer(delay).timeout
+	$AnimationPlayer.play("in")
+	
+func play_discard():
+	$AnimationPlayer.play("discard")
+	
+func select_card():
+	ignore_input = true
+	$AnimationPlayer.play("selected")
+	
+	for other_card in get_tree().get_nodes_in_group("upgrade_card"):
+		if other_card != self:
+			other_card.play_discard()
+	
+	await $AnimationPlayer.animation_finished
+	selected.emit()
 
 func set_ability_upgrade(upgrade : AbilityUpgrade):
 	name_label.text = upgrade.name
 	description_label.text = upgrade.description
 	
 func on_gui_input(event : InputEvent):
+	if ignore_input:
+		return
+		
 	if event.is_action_pressed("left_click"):
-		selected.emit()
+		select_card()
+		
+func on_mouse_entered():
+	if ignore_input:
+		return
+		
+	$HoverAnimationPlayer.play("hover")
--- scenes/UI/upgrade_screen.gd
+++ scenes/UI/upgrade_screen.gd
@@ -11,10 +11,13 @@ func _ready():
 	get_tree().paused = true
 
 func set_ability_upgrades(upgrades : Array[AbilityUpgrade]):
+	var delay = 0.0
 	for upgrade in upgrades:
 		var card_instance : AbilityUpgradeCard = upgrade_card_scene.instantiate()
 		card_container.add_child(card_instance)
 		card_instance.set_ability_upgrade(upgrade)
+		card_instance.play_in(delay)
+		delay += 0.2
 		card_instance.selected.connect(on_upgrade_selected.bind(upgrade))
 		
 func on_upgrade_selected(upgrade : AbilityUpgrade):
